25eb1b83a795055df1e231cb3029e72152e9d214
nalim07 <alimdulah@gmail.com>
2025-02-05 17:30:16 +0700

    feat: add logo

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" href="{{ asset('images/logo.ico') }}">
+
     <title>{{ $title ?? Route::currentRouteName() }}</title>
 
     @livewireStyles
